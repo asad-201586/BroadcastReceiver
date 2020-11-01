3796f3b65deeab308495271bbc9e82239c1c05bb
asad-201586 <asadul201586@gmail.com>
2020-11-01 23:01:53 +0600

    custom broadcast added

--- app/src/main/java/com/asad/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/asad/broadcastreceiver/MainActivity.java
@@ -2,25 +2,33 @@ package com.asad.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.view.View;
+import android.widget.TextView;
 
 import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
 
     Broadcast broadcast = new Broadcast();
+    private TextView msgTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        msgTV = findViewById(R.id.textView);
+
     }
 
-    @Override
+   /* @Override
     protected void onStart() {
         super.onStart();
         IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
@@ -33,5 +41,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         unregisterReceiver(broadcast);
+    }*/
+
+    public void broadcast(View view) {
+        Intent intent = new Intent("com.asad.TEST_ACTION");
+        intent.putExtra("com.asad.EXAMPLE_TEXT","Broadcast Received");
+        sendBroadcast(intent);
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String msg = intent.getStringExtra("com.asad.EXAMPLE_TEXT");
+            msgTV.setText(msg);
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter("com.asad.TEST_ACTION");
+        registerReceiver(broadcastReceiver,intentFilter);
     }
 }
